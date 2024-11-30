30dae555be27148545d596ee8b5867047c60ee5a
mategor195 <mategor195@student.polsl.pl>
2024-11-30 19:34:39 +0100

    PrisonMerchant

--- PrisonLoop/Assets/Scripts/PrisonerMerchant/PrisonerMerchant.cs
+++ PrisonLoop/Assets/Scripts/PrisonerMerchant/PrisonerMerchant.cs
@@ -3,46 +3,58 @@ using UnityEngine;
 public class PrisonerMerchant : MonoBehaviour, IInteractable
 {
     public TradeOffer tradeOffer;
+    public TradeUI tradeUI;
     SpriteRenderer spriteRenderer;
-    TradeUI tradeUI;
-    
+    private bool boughtOut;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        tradeUI = GetComponentInChildren<TradeUI>();
+        tradeUI.SetUp(tradeOffer);
     }
 
+
+    void Start()
+    {
+        tradeUI.ShowUI();
+    }
     public void Interact()
     {
+        if (boughtOut)
+        {
+            spriteRenderer.color = Color.red;
+            Debug.Log("Bought out");
+            return;
+        }
+        spriteRenderer.color = Color.green;
         //Try to buy item with every item form Eq 
-        
-        if(GameManager.Instance.PlayerEq.ContainsItem(tradeOffer.desiredItem));
-        
-        throw new System.NotImplementedException();
+        PlayerEq playerEq = GameManager.Instance.PlayerEq;
+        Debug.Log(playerEq.ContainsItem(tradeOffer.desiredItem.Type));
+
+        if (playerEq.ContainsItem(tradeOffer.desiredItem.Type))
+        {
+            if (playerEq.removeItem(tradeOffer.desiredItem) &&
+                playerEq.addItem(tradeOffer.offeredItem))
+            {
+                boughtOut = true;
+                tradeUI.HideUI();
+            }
+        }
+            
     }
 
     public void OnPlayerEnter()
     {
         //Show u interacted with him 
         spriteRenderer.color = Color.yellow;
-        throw new System.NotImplementedException();
     }
 
     public void OnPlayerExit()
     {
         //Hide interacted with him
         spriteRenderer.color = Color.white;
-        throw new System.NotImplementedException();
     }
 
-    private bool TryToTrade(ItemEnum itemEnum)
-    {
-        if (itemEnum == tradeOffer.desiredItem)
-        {
-            return true;
-        }
-        return false;
-    }
     
 }

25f2f351ce352fe06dacbc6bff1e6676a23efb8b
mategor195 <mategor195@student.polsl.pl>
2024-12-01 03:44:32 +0100

    helper

--- PrisonLoop/Assets/Scripts/Entities/GuardFOV.cs
+++ PrisonLoop/Assets/Scripts/Entities/GuardFOV.cs
@@ -58,7 +58,11 @@ public class GuardFOV : MonoBehaviour
     {
         //TODO implement contraband check
         //1. check if player has contraband in inventory startTimer 
+        if(GameManager.Instance.PlayerEq.ContainsContraband())
+            return true;
+        
         //2. if digidig 
+        return false
     }
 
     private bool IsTargetInFieldOfView()
